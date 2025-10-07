402684be1fadc413a4916892db85e4f23c4a01e5
rocasoftw <92948053+rocasoftw@users.noreply.github.com>
2025-10-07 15:08:15 -0300

    Delete pages directory

--- pages/Login.js
+++
@@ -1,2 +0,0 @@
-// This file has been neutralized to prevent conflicts with its TypeScript counterpart (Login.tsx).
-// The application uses in-browser transpilation of .tsx files.
--- pages/PatientsList.js
+++
@@ -1,2 +0,0 @@
-// This file has been neutralized to prevent conflicts with its TypeScript counterpart (PatientsList.tsx).
-// The application uses in-browser transpilation of .tsx files.
